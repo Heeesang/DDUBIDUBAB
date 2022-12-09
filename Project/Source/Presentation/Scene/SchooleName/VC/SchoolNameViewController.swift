c33765fd8c07c20d0beaadeb6512bab31df85999
Heesang <ghs050611@naver.com>
2022-12-09 18:09:59 +0900

    :lipstick: SchoolNameVC UI

--- Project/Source/Presentation/Scene/SchooleName/VC/SchoolNameViewController.swift
+++ Project/Source/Presentation/Scene/SchooleName/VC/SchoolNameViewController.swift
@@ -4,5 +4,60 @@ import SnapKit
 import Lottie
 
 class SchoolNameViewController: BaseVC<SchoolNameViewModel> {
+    private let mainLottieAnimationView = LottieAnimationView(name: "dancing-monkey").then {
+        $0.contentMode = .scaleAspectFit
+        $0.loopMode = .loop
+        $0.play()
+    }
     
+    private let mainTitleLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 18, weight: .heavy)
+        $0.text = "DDUBIDUBAB"
+    }
+    
+    private let schoolNameTextField = UITextField().then {
+        $0.layer.borderColor = UIColor.black.cgColor
+        $0.layer.borderWidth = 1.0
+        $0.addLeftPadding()
+        $0.placeholder = "학교 이름을 입력해 주세요."
+    }
+    
+    private lazy var nextButton = UIButton().then {
+        $0.setTitle("검색", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 15, weight: .bold)
+        $0.layer.borderColor = UIColor.black.cgColor
+        $0.layer.borderWidth = 1.0
+    }
+    
+    override func addView() {
+        view.addSubViews( mainTitleLabel, mainLottieAnimationView, schoolNameTextField, nextButton)
+    }
+    
+    override func setLayout() {
+        mainLottieAnimationView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(60)
+            $0.height.equalTo(120)
+            $0.centerX.equalToSuperview()
+        }
+        
+        mainTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(mainLottieAnimationView.snp.bottom)
+            $0.centerX.equalToSuperview()
+        }
+        
+        schoolNameTextField.snp.makeConstraints {
+            $0.top.equalTo(mainTitleLabel.snp.bottom).offset(50)
+            $0.height.equalTo(40)
+            $0.centerX.equalToSuperview()
+            $0.leading.equalToSuperview().offset(35)
+        }
+        
+        nextButton.snp.makeConstraints {
+            $0.height.equalTo(50)
+            $0.bottom.equalToSuperview().inset(60)
+            $0.centerX.equalToSuperview()
+            $0.leading.equalToSuperview().offset(35)
+        }
+    }
 }
