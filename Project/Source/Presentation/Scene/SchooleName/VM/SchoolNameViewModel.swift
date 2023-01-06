ee1f33bb1ee9fae223ef5a038dd3add194023644
Heesang <ghs050611@naver.com>
2023-01-06 17:52:02 +0900

    :sparkles: schoolInfo API 연결

--- Project/Source/Presentation/Scene/SchooleName/VM/SchoolNameViewModel.swift
+++ Project/Source/Presentation/Scene/SchooleName/VM/SchoolNameViewModel.swift
@@ -1,7 +1,26 @@
 import UIKit
+import Moya
 
 class SchoolNameViewModel: BaseViewModel {
     
-    var schoolAddress: [String] = ["비아동", "GSM"]
+    var schoolAddress: [schoolsInfo] = []
     
+    func fetchSchoolName() {
+        let provider = MoyaProvider<SchoolNameAPI>()
+        provider.request(.schools(name: "", apiKey: "e6f3e10be1b1426cbcfb2be62afff409")) { (result) in
+            switch result {
+            case .success(let response):
+                let responseData = response.data
+                do {
+                    let decoded = try JSONDecoder().decode(schoolResponse.self, from: responseData )
+                    self.schoolAddress = decoded.schools
+                    print(decoded.schools)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
